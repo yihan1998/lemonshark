667410934095a2659cbcaacc3b6c2412c39fcc25
yihan <yangyihan1998@gmail.com>
2024-12-22 09:00:41 +0000

    integrating bullshark benchmark

---
+++ benchmark/fabfile.py
@@ -0,0 +1,37 @@
+# Copyright(C) Facebook, Inc. and its affiliates.
+from fabric import task
+
+from benchmark.local import LocalBench
+from benchmark.logs import ParseError, LogParser
+from benchmark.utils import Print
+from benchmark.plot import Ploter, PlotError
+from benchmark.instance import InstanceManager
+from benchmark.remote import Bench, BenchError
+
+
+@task
+def local(ctx, debug=True):
+    ''' Run benchmarks on localhost '''
+    bench_params = {
+        'faults': 0,
+        'nodes': 4,
+        'workers': 1,
+        'rate': 50_000,
+        'tx_size': 512,
+        'duration': 20,
+    }
+    node_params = {
+        'header_size': 50,  # bytes
+        'max_header_delay': 1_000,  # ms
+        'gc_depth': 50,  # rounds
+        'sync_retry_delay': 10_000,  # ms
+        'sync_retry_nodes': 3,  # number of nodes
+        'batch_size': 500_000,  # bytes
+        'max_batch_delay': 200  # ms
+    }
+    try:
+        ret = LocalBench(bench_params, node_params).run(debug)
+        print(ret.result())
+    except BenchError as e:
+        Print.error(e)
+
